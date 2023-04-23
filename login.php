4e02fe18ca1101ad9ac46db57cbd2d11415a1ceb
[Adrien Kouyoumjian] <[adrien.kouyoumjian@outlook.fr]>
2023-04-23 13:19:58 +0200

    login related to register

---
+++ login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Login Form</title>
+</head>
+<body>
+    <div class="container">
+        <?php 
+        if(isset($_POST["login"])){
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            require_once "database.php";
+            $sql = "SELECT * FROM users WHERE email = '$email'";
+            $result = mysqli_query($conn, $sql);
+            $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            if($user){
+                if(password_verify($password, $user["password"])){
+                    header("Location: index.php");
+                    die();
+                }else {
+                    echo "<div class='alert alert-danger'>Password does not match </div>";
+                }
+            }else {
+                echo "<div class='alert alert-danger'>Email does not match </div>";
+            }
+        }
+        ?>
+        <form action="login.php" method="post">
+            <div class="form-group">
+                <input type="email" placeholder="Enter Email:" name="email" class="form-control">
+            </div>
+            <div class="form-group">
+                <input type="password" placeholder="Enter Password:" name="password" class="form-control">
+            </div>
+            <div class="form-btn">
+                <input type="submit" value="Login" name="login" class="btn btn-primary">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
